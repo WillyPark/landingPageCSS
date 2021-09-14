7f6fb1ca2a8cc05dc6e00cef1ef421ca920eea4c
William Park <l.jimenez.leon97@gmail.com>
2021-09-14 12:32:27 -0500

    First Commit

---
+++ js/menu.js
@@ -0,0 +1,14 @@
+(function() {
+    const opentButton = document.querySelector(".nav__menu");
+    const menu = document.querySelector(".nav__link");
+    const closeMenu = document.querySelector(".nav__close");
+
+    opentButton.addEventListener("click", () => {
+        menu.classList.add("nav__link--show");
+    });
+    
+    closeMenu.addEventListener("click", () => {
+        menu.classList.remove("nav__link--show");
+    });
+
+})();
